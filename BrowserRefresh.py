02e391af0ca19f8d18e1884c9984b9e1aa7525fe
Michael Camden <camden.michael@gmail.com>
2012-06-26 19:43:27 -0700

    Remove system events usage, replace with original command
    Found keystrokes to be problematic when activating from python shell.
    Reverting to previous command structure, but added support for Safari
    browser. Firefox does not yet support reloading of tabs in Applescript,
    as far as I am aware.

--- BrowserRefresh.py
+++ BrowserRefresh.py
@@ -6,21 +6,28 @@ class BrowserRefreshCommand(sublime_plugin.TextCommand):
     def run(self, args, activate_browser=True, browserName="Google Chrome"):
         if self.view and self.view.is_dirty():
             self.view.run_command("save")
+        
+        activate = ""
+        if(activate_browser == True):
+            activate = "activate"
 
-        # ApplicationIsRunnning from, http://vgable.com/blog/2009/04/24/how-to-check-if-an-application-is-running-with-applescript/
-        browser_command = """
-        on ApplicationIsRunning(appName)
-            tell application "System Events" to set appNameIsRunning to exists (processes where name is appName)
-            return appNameIsRunning
-        end ApplicationIsRunning
+        if(browserName == "Google Chrome"):
+            command = """
+                tell application "Google Chrome"
+                    %s
+                    reload active tab of window 1
+                end tell
+            """ % (activate)
 
-        if ApplicationIsRunning("%s")
-            tell application "%s" to activate
-            tell application "System Events"
-                delay .5
-                keystroke "r" using {command down}
-            end tell
-        end if
-        """ % (browserName, browserName)
+        elif(browserName == "Safari"):
+            command = """
+                tell application "Safari"
+                    %s
+                    tell its first document
+                        set its URL to (get its URL)
+                    end tell
+                end tell
+            """ % (activate)
 
-        call(['osascript', '-e', browser_command])
+        call(['osascript', '-e', command])
+
