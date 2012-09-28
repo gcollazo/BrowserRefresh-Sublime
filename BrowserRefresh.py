3cf881557e109c56d0e3b98719e61e5e97cbd0f4
Giovanni Collazo <gcollazo@gmail.com>
2012-09-27 23:56:36 -0400

    Adds auto_save option, reorganizes stuff
    - Makes it possible to turn off save before the refresh
    - Moves browser commands to functions
    - Adds a function for the osascript call

--- BrowserRefresh.py
+++ BrowserRefresh.py
@@ -3,47 +3,73 @@ from subprocess import call
 
 
 class BrowserRefreshCommand(sublime_plugin.TextCommand):
-    def run(self, args, activate_browser=True, browserName="Google Chrome"):
-        if self.view and self.view.is_dirty():
+    def run(self, args, activate_browser=True, browser_name="Google Chrome", auto_save=True):
+        if auto_save == True and self.view and self.view.is_dirty():
             self.view.run_command("save")
-        
-        activate = ""
-        if(activate_browser == True):
-            activate = "activate"
 
-        if(browserName == "Google Chrome"):
-            command = """
-                tell application "Google Chrome"
-                    %s
-                    reload active tab of window 1
+        self.activate = ""
+        if activate_browser == True:
+            self.activate = "activate"
+
+        if browser_name == "Google Chrome":
+            self._chrome()
+
+        elif browser_name == "Safari":
+            self._safari()
+
+        elif browser_name == "Firefox":
+            self._firefox()
+
+        elif browser_name == "Opera":
+            self._opera()
+
+        elif browser_name == 'all':
+            self._chrome()
+            self._safari()
+            self._firefox()
+            self._opera()
+
+    def _chrome(self):
+        command = """
+            tell application "Google Chrome"
+                %s
+                reload active tab of window 1
+            end tell
+        """ % (self.activate)
+
+        self._call_applescript(command)
+
+    def _safari(self):
+        command = """
+            tell application "Safari"
+                %s
+                tell its first document
+                    set its URL to (get its URL)
                 end tell
-            """ % (activate)
+            end tell
+        """ % (self.activate)
 
-        elif(browserName == "Safari"):
-            command = """
-                tell application "Safari"
-                    %s
-                    tell its first document
-                        set its URL to (get its URL)
-                    end tell
-                end tell
-            """ % (activate)
+        self._call_applescript(command)
 
-        elif(browserName == "Firefox"):
-            command = """
-                tell application "Firefox"
-                    %s
-                    tell application "System Events" to keystroke "r" using command down
-                end tell
-            """ % (activate)
+    def _firefox(self):
+        command = """
+            tell application "Firefox"
+                activate
+                tell application "System Events" to keystroke "r" using command down
+            end tell
+        """
 
-        elif(browserName == "Opera"):
-            command = """
-                tell application "Opera"
-                    %s
-                    tell application "System Events" to keystroke "r" using command down
-                end tell
-            """ % (activate)    
+        self._call_applescript(command)
 
+    def _opera(self):
+        command = """
+            tell application "Opera"
+                activate
+                tell application "System Events" to keystroke "r" using command down
+            end tell
+        """
+
+        self._call_applescript(command)
+
+    def _call_applescript(self, command):
         call(['osascript', '-e', command])
-
