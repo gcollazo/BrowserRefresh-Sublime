61cca60be5d3243cd93d2fa1d2e285d9248d709b
Giovanni Collazo <gcollazo@gmail.com>
2013-03-17 21:56:28 -0400

    Check for active browsers every time the command runs

--- BrowserRefresh.py
+++ BrowserRefresh.py
@@ -5,6 +5,7 @@ import platform
 import sublime
 import sublime_plugin
 
+
 # Fix windows imports
 __file__ = os.path.normpath(os.path.abspath(__file__))
 __path__ = os.path.dirname(__file__)
@@ -22,17 +23,17 @@ _os = platform.system()
 
 class BrowserRefreshCommand(sublime_plugin.TextCommand):
     def run(self, args, activate_browser=True,
-        browser_name="all", auto_save=True,
-        delay=None):
+            browser_name='all', auto_save=True, delay=None):
 
         # Auto-save
-        if auto_save == True and self.view and self.view.is_dirty():
-            self.view.run_command("save")
+        if auto_save and self.view and self.view.is_dirty():
+            self.view.run_command('save')
 
         # Detect OS and import
         if _os == 'Darwin':
             from mac import MacBrowserRefresh
-            refresher = MacBrowserRefresh(activate_browser)
+            from mac.utils import running_browsers
+            refresher = MacBrowserRefresh(activate_browser, running_browsers())
         elif _os == 'Windows':
             from win import WinBrowserRefresh
             refresher = WinBrowserRefresh(activate_browser)
@@ -45,19 +46,19 @@ class BrowserRefreshCommand(sublime_plugin.TextCommand):
             time.sleep(delay)
 
         # Actually refresh browsers
-        if browser_name == "Google Chrome":
+        if browser_name == 'Google Chrome':
             refresher.chrome()
 
-        elif browser_name == "Google Chrome Canary" and _os == 'Darwin':
+        elif browser_name == 'Google Chrome Canary' and _os == 'Darwin':
             refresher.canary()
 
-        elif browser_name == "Safari":
+        elif browser_name == 'Safari':
             refresher.safari()
 
-        elif browser_name == "Firefox":
+        elif browser_name == 'Firefox':
             refresher.firefox()
 
-        elif browser_name == "Opera":
+        elif browser_name == 'Opera':
             refresher.opera()
 
         elif browser_name == 'IE' and _os == 'Windows':
